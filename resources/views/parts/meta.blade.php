4f3393f1cb905dd15b1d11e8ede212410024716f
Cesar Augusto Quijada <c.qmacuart@gmail.com>
2020-09-30 13:20:20 -0500

    fixing root image meta tag

--- resources/views/parts/meta.blade.php
+++ resources/views/parts/meta.blade.php
@@ -28,22 +28,23 @@
 <meta property="og:title" content="{{$metaInfo['site']['storename']}}">
 <meta property="og:site_name" content="{{$metaInfo['site']['storename']}}">
 <meta property="og:url" content="{{ url('/') }}">
-<meta property="og:description" content="mi pequeña tienda">
-<meta property="og:type" content="website">
+<meta property="og:description" content="Mi pequeña tienda">
+<meta property="og:type" content="article">
 <meta property="og:image" content="{{ url('/img/settings/'.$metaInfo['site']['image']) }}">
 
 <!-- Open Graph / Facebook -->
-<meta property="og:type" content="website">
+<meta property="og:type" content="article">
 <meta property="og:url" content="{{ url('/') }}">
 <meta property="og:title" content="{{$metaInfo['site']['storename']}}">
-<meta property="og:description" content="mi pequeña tienda">
+<meta property="og:description" content="Mi pequeña tienda">
+<meta property="og:image:width" content="150" />
 <meta property="og:image" content="{{ url('/img/settings/'.$metaInfo['site']['image']) }}">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="{{ url('/') }}">
 <meta property="twitter:title" content="{{$metaInfo['site']['storename']}}">
-<meta property="twitter:description" content="mi pequeña tienda">
+<meta property="twitter:description" content="Mi pequeña tienda">
 <meta property="twitter:image" content="{{ url('/img/settings/'.$metaInfo['site']['image']) }}">
     
 @endif
