53901fd64d4618b17efc8be34eda04e6e0079166
Cesar Augusto Quijada <c.qmacuart@gmail.com>
2020-10-05 02:32:36 -0500

    Add fb business generator file

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use App\Product;
 use App\Category;
 use App\Tipo;
@@ -179,6 +180,7 @@ class ProductsController extends Controller
         //
         $entrada = $request->all();
         if (Product::create($entrada)) {
+            $this->builtFBCSV();
             return response()->json($entrada, 200);
         } else {
             return response()->json($entrada, 400);
@@ -224,6 +226,7 @@ class ProductsController extends Controller
         $producto = Product::findOrFail($id);
         $entrada = $request->all();
         $producto->update($entrada);
+        $this->builtFBCSV();
         return response()->json($producto, 200);
     }
 
@@ -238,6 +241,37 @@ class ProductsController extends Controller
         //
         $producto = Product::findOrFail($id);
         $producto->delete();
+        $this->builtFBCSV();
         return response()->json($producto, 200);
+    }
+
+    public function builtFBCSV()
+    {
+        $productos = Product::all();
+        $ajustes = \App\Setting::first();
+        //Añadir primera fila de encabezados al arreglo
+        $encabezados = ["id", "title", "description", "availability", "inventory", "condition", "price", "link", "image", "brand"];
+        $csvProductsArray[] = $encabezados;
+        if ($productos) {
+            //crear arreglo de información que irá al csv
+            foreach ($productos as $value) {
+                $csvProductsArray[] = [$value->id, htmlentities($value->nombre), htmlentities($value->short_des), "in stock", 100, "new", $value->price . " " . $ajustes->currency, "'" . URL::to("/") . '/producto/' . $value->id . "/'", "'" . URL::to("/") . "/img/products/" . rawurlencode($value->image) . "/'", "no-brand"];
+            }
+            //ARCHIVO DELIMITADO POR PUNTO Y COMA (;)
+            //variables básicas: Titulo del archivo para FB
+            //crear directorio si no existe
+            $docTitle = "files/fb_business/facebook_business_product_csv.csv";
+            $ruta = $docTitle;
+            $delimitador = ",";
+            $encapsulador = '"';
+
+            // Generar el archivo linea a linea
+            $file_handle = fopen($ruta, "w");
+            foreach ($csvProductsArray as $value) {
+                fputcsv($file_handle, $value, $delimitador, $encapsulador);
+            }
+            rewind($file_handle);
+            fclose($file_handle);
+        }
     }
 }
